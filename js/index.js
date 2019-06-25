7cc53bd3aa6de6a4964a0dd958960f2c82462105
Robert McCary <robert.mccary@yahoo.com>
2019-06-25 13:34:50 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-function getRepositories(){
-  const username = document.getElementById('username').value;
-  const req = new XMLHttpRequest
-  req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/${username}/repos');
-  req.send();
+function getRepositories() {
+  const username = document.getElementById('username').value
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', displayRepositories)
+  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  req.send()
 }
 
 function displayRepositories(){
